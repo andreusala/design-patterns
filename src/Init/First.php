e28f6e92f26dc49a777437fdf1ffc16e66f4ba32
Andreu Sala <andreu.sala@onestic.com>
2022-10-25 18:31:11 +0200

    #1 Add docker images and init composer

---
+++ src/Init/First.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace DesignPatterns\Init;
+
+class First
+{
+    /**
+     * @return bool
+     */
+    public function execute(): bool
+    {
+        return true;
+    }
+}
